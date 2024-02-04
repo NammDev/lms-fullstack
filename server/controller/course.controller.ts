038b6343437a7b07755f2de9bc98513107e8f0f3
Nam <namdeveloper.ca@gmail.com>
2024-02-04 15:16:07 +0700

    save courses public info to redis

--- server/controller/course.controller.ts
+++ server/controller/course.controller.ts
@@ -5,6 +5,7 @@ import { CatchAsyncError } from '../middleware/catchAsyncError'
 import ErrorHandler from '../utils/ErrorHandler'
 import { createCourse } from '../services/course.service'
 import courseModel from '../models/course.model'
+import { redis } from '../utils/redis'
 
 require('dotenv').config()
 
@@ -66,15 +67,26 @@ export const editCourse = CatchAsyncError(
 export const getSingleCourse = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const course = await courseModel
-        .findById(req.params.id)
-        .select(
-          '-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links'
-        )
-      res.status(200).json({
-        success: true,
-        course,
-      })
+      const courseId = req.params.id
+      const isCacheExist = await redis.get(courseId)
+      if (isCacheExist) {
+        const course = JSON.parse(isCacheExist)
+        res.status(200).json({
+          success: true,
+          course,
+        })
+      } else {
+        const course = await courseModel
+          .findById(req.params.id)
+          .select(
+            '-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links'
+          )
+        await redis.set(courseId, JSON.stringify(course))
+        res.status(200).json({
+          success: true,
+          course,
+        })
+      }
     } catch (error: any) {
       return next(new ErrorHandler(500, error.message))
     }
@@ -85,15 +97,25 @@ export const getSingleCourse = CatchAsyncError(
 export const getAllCourse = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const courses = await courseModel
-        .find()
-        .select(
-          '-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links'
-        )
-      res.status(200).json({
-        success: true,
-        courses,
-      })
+      const isCacheExist = await redis.get('allCourses')
+      if (isCacheExist) {
+        const courses = JSON.parse(isCacheExist)
+        res.status(200).json({
+          success: true,
+          courses,
+        })
+      } else {
+        const courses = await courseModel
+          .find()
+          .select(
+            '-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links'
+          )
+        await redis.set('allCourses', JSON.stringify(courses))
+        res.status(200).json({
+          success: true,
+          courses,
+        })
+      }
     } catch (error: any) {
       return next(new ErrorHandler(500, error.message))
     }
