6e2b51b1fe8d8332628599d21cbc8af1139ed2eb
Nam <namdeveloper.ca@gmail.com>
2024-02-04 13:17:11 +0700

    edit course

--- server/controller/course.controller.ts
+++ server/controller/course.controller.ts
@@ -4,6 +4,7 @@ import { NextFunction, Request, Response } from 'express'
 import { CatchAsyncError } from '../middleware/catchAsyncError'
 import ErrorHandler from '../utils/ErrorHandler'
 import { createCourse } from '../services/course.service'
+import courseModel from '../models/course.model'
 
 require('dotenv').config()
 
@@ -32,3 +33,31 @@ export const uploadCourse = CatchAsyncError(
 )
 
 // edit course
+export const editCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body
+      const thumbnail = data.thumbnail
+      if (thumbnail) {
+        await cloudinary.v2.uploader.destroy(thumbnail.public_id)
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: 'courses',
+        })
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        }
+      }
+
+      // get course ID
+      const courseId = req.params.id
+      const course = await courseModel.findByIdAndUpdate(courseId, { $set: data }, { new: true })
+      res.status(201).json({
+        success: true,
+        course,
+      })
+    } catch (error: any) {
+      return next(new ErrorHandler(500, error.message))
+    }
+  }
+)
