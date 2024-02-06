5a2205bb8a269aafc755c71489f4bd44a671d054
Nam <namdeveloper.ca@gmail.com>
2024-02-06 09:58:38 +0700

    create orderModel & notificationModel

---
+++ server/models/notification.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document, Model, Schema } from 'mongoose'
+import { IUser } from './user.model'
+
+export interface INotification extends Document {
+  title: string
+  message: string
+  status: string
+  userId: string
+}
+
+const notificationSchema = new Schema<INotification>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: 'unread',
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+const notificationModel: Model<INotification> = mongoose.model('Notification', notificationSchema)
+export default notificationModel
