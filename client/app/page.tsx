c52895826895d35103a9da6434b5d4fe78a78282
Nam <namdeveloper.ca@gmail.com>
2024-02-17 18:11:49 +0700

    Heading

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,17 @@
 'use client'
 
+import Heading from './utils/Heading'
+
 interface Props {}
 
-const Page: React.FC<Props> = () => {
+const Page = (props: Props) => {
   return (
     <div>
+      <Heading
+        title='E Learning'
+        description='E-Learning is a platform for students to learn and get help from teachers'
+        keywords='programming,nextjs,mern,react,reactjs'
+      />
       <h1>Page</h1>
     </div>
   )
