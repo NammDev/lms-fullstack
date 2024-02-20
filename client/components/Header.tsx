4e8786b8ed9fcc92b609b8e7e4b4a4462c6ab895
Nam <namdeveloper.ca@gmail.com>
2024-02-20 19:21:37 +0700

    header menu & mobile

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -5,6 +5,9 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import NavItems from '../utils/NavItem'
 import { ThemeSwitcher } from '../utils/ThemeSwitcher'
+import Image from 'next/image'
+import { HiOutlineMenuAlt3, HiOutlineUserCircle } from 'react-icons/hi'
+import avatar from '@/public/assests/avatar.png'
 
 type Props = {
   open: boolean
@@ -16,6 +19,8 @@ const Header = ({ open, setOpen, activeItem }: Props) => {
   const [active, setActive] = useState(false)
   const [openSidebar, setOpenSidebar] = useState(false)
 
+  const userData: any = {}
+
   if (typeof window !== 'undefined') {
     window.addEventListener('scroll', () => {
       if (window.scrollY > 80) {
@@ -24,6 +29,14 @@ const Header = ({ open, setOpen, activeItem }: Props) => {
         setActive(false)
       }
     })
+  }
+
+  const handleClose = (e: any) => {
+    if (e.target.id === 'screen') {
+      {
+        setOpenSidebar(false)
+      }
+    }
   }
 
   return (
@@ -51,9 +64,67 @@ const Header = ({ open, setOpen, activeItem }: Props) => {
             <div className='flex items-center'>
               <NavItems activeItem={activeItem} isMobile={false} />
               <ThemeSwitcher />
+              {/* only for mobile */}
+              <div className='800px:hidden'>
+                <HiOutlineMenuAlt3
+                  size={25}
+                  className='cursor-pointer dark:text-white text-black'
+                  onClick={() => setOpenSidebar(true)}
+                />
+              </div>
+              {/* User Profile */}
+              {userData?.user ? (
+                <Link href={'/profile'}>
+                  <Image
+                    src={userData?.user.avatar ? userData.user.avatar.url : avatar}
+                    alt=''
+                    width={30}
+                    height={30}
+                    className='w-[30px] h-[30px] rounded-full cursor-pointer'
+                    style={{ border: activeItem === 5 ? '2px solid #37a39a' : 'none' }}
+                  />
+                </Link>
+              ) : (
+                <HiOutlineUserCircle
+                  size={25}
+                  className='hidden 800px:block cursor-pointer dark:text-white text-black'
+                  onClick={() => setOpen(true)}
+                />
+              )}
             </div>
           </div>
         </div>
+
+        {/* mobile sidebar */}
+        {openSidebar && (
+          <div
+            className='fixed w-full h-screen top-0 left-0 z-[99999] dark:bg-[unset] bg-[#00000024]'
+            onClick={handleClose}
+            id='screen'
+          >
+            <div className='w-[70%] fixed z-[999999999] h-screen bg-white dark:bg-slate-900 dark:bg-opacity-90 top-0 right-0'>
+              <NavItems activeItem={activeItem} isMobile={true} />
+              {userData?.user ? (
+                <Link href={'/profile'}>
+                  <Image
+                    src={userData?.user.avatar ? userData.user.avatar.url : avatar}
+                    alt=''
+                    width={30}
+                    height={30}
+                    className='w-[30px] h-[30px] rounded-full ml-[20px] cursor-pointer'
+                    style={{ border: activeItem === 5 ? '2px solid #37a39a' : 'none' }}
+                  />
+                </Link>
+              ) : (
+                <HiOutlineUserCircle
+                  size={25}
+                  className='hidden 800px:block cursor-pointer dark:text-white text-black'
+                  onClick={() => setOpen(true)}
+                />
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
